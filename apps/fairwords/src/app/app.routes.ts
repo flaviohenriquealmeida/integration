06fa4ee035dd5aa83e1a4b314dd8b09286befa08
Flavio Almeida <flavio.almeida@mycomplianceoffice.com>
2023-09-25 08:30:56 +0100

    relative imports

--- apps/fairwords/src/app/app.routes.ts
+++ apps/fairwords/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [{
   path: 'feature1',
-  loadChildren: () => import('apps/fairwords/src/app/feature1/feature1.module').then(m => m.Feture1Module)
+  loadChildren: () => import('./feature1/feature1.module').then(m => m.Feture1Module)
 }];
